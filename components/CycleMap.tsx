8463477d07cad4be79ce7e08092c4828e86d7634
phghezzoclaudio <info.phghezzoclaudio@gmail.com>
2026-03-05 22:53:04 +0100

    CycleMap

--- components/CycleMap.tsx
+++ components/CycleMap.tsx
@@ -1,32 +1,43 @@
 "use client"
 
-import { MapContainer, TileLayer, GeoJSON, Marker } from "react-leaflet"
+import { MapContainer, TileLayer, GeoJSON, Marker, useMap } from "react-leaflet"
 import { useEffect, useState } from "react"
-import * as turf from "@turf/turf"
 import { FeatureCollection } from "geojson"
 
-type Props={
- slug:string
+type Props = {
+ slug: string
 }
 
-export default function CycleMap({slug}:Props){
+function FixMap() {
+ const map = useMap()
 
- const [route,setRoute] = useState<FeatureCollection | null>(null)
- const [position,setPosition] = useState<[number,number] | null>(null)
+ useEffect(() => {
+  setTimeout(() => {
+   map.invalidateSize()
+  }, 100)
+ }, [map])
 
- useEffect(()=>{
+ return null
+}
+
+export default function CycleMap({ slug }: Props) {
+
+ const [route, setRoute] = useState<FeatureCollection | null>(null)
+ const [position, setPosition] = useState<[number, number] | null>(null)
+
+ useEffect(() => {
 
   fetch(`/Cycleroutes/${slug}.geojson`)
-   .then(r=>r.json())
+   .then(r => r.json())
    .then(setRoute)
 
- },[slug])
+ }, [slug])
 
- useEffect(()=>{
+ useEffect(() => {
 
-  if(!navigator.geolocation) return
+  if (!navigator.geolocation) return
 
-  const watch=navigator.geolocation.watchPosition(pos=>{
+  const watch = navigator.geolocation.watchPosition(pos => {
 
    setPosition([
     pos.coords.latitude,
@@ -35,28 +46,29 @@ export default function CycleMap({slug}:Props){
 
   })
 
-  return ()=>navigator.geolocation.clearWatch(watch)
+  return () => navigator.geolocation.clearWatch(watch)
 
- },[])
+ }, [])
 
- return(
+ return (
 
   <MapContainer
-   center={[45.44,12.33]}
+   center={[45.44, 12.33]}
    zoom={10}
-   style={{height:"70vh",width:"100%"}}
+   style={{ height: "70vh", width: "100%" }}
   >
+
+   <FixMap />
 
    <TileLayer
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
    />
 
-   {route && <GeoJSON data={route}/>}
+   {route && <GeoJSON data={route} />}
 
-   {position && <Marker position={position}/>}
+   {position && <Marker position={position} />}
 
   </MapContainer>
 
  )
-
 }
